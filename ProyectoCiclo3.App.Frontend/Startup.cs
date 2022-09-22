c31aab89e431673acf9d0cbc5d2338ad1e97656e
ldelaossa23 <lda23@hotmail.com>
2022-09-21 20:55:43 -0500

    repositoriosem4

--- ProyectoCiclo3.App.Frontend/Startup.cs
+++ ProyectoCiclo3.App.Frontend/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ProyectoCiclo3.App.Persistencia.AppRepositorios;
 
 namespace ProyectoCiclo3.App.Frontend
 {
@@ -24,6 +25,9 @@ namespace ProyectoCiclo3.App.Frontend
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddSingleton<RepositorioEncomiendas,RepositorioEncomiendas>();
+            // services.AddSingleton<RepositorioUsuarios,RepositorioUsuarios>();
+            // services.AddSingleton<RepositorioServicios,RepositorioServicios>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
